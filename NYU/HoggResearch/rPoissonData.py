cea84161cd5b4bf5c9acea22a76467cb99b034b8
Jeffrey Gertler <gertler2822@yahoo.com>
2012-02-29 21:31:59 -0500

    Added histogram and random number seed

--- NYU/HoggResearch/rPoissonData.py
+++ NYU/HoggResearch/rPoissonData.py
@@ -4,12 +4,10 @@ import math
 import random
 import sys
 
-#input of data from argparse
 
+# input of data from argparse
+# args = initial gamma, dt, d/dt gamma, random generator seed
 args = sys.argv
-
-print(args[1])
-print(args[2])
 
 gamma = float(args[1])
 dt = float(args[2])
@@ -18,25 +16,37 @@ n = gamma * dt
 print("Ideal photon count: " + str(n))
 
 
-# Generates k value for 
+# Generates k value for
+random.seed(args[3])
 kTest = np.random.poisson(n, 1)
 print("Generated k value: " + str(kTest))
 print("Gamma calculated from chosen k: " + str(kTest/dt))
 
-#generate fake set of data making k time data points between 0 and dt
+# generate fake set of data making k time data points between 0 and dt
+# usses a different seed each time but is based on the seed passed in args
 tData = np.zeros(kTest)
 for i in range(1, kTest):
+    random.seed(args[3]*i)
     tData[i] = random.random() * dt
 print(tData)
 
-#plot for generated data of photons
+# plot for generated data of photons
 tDataY = np.zeros(kTest)
+
+plt.figure(1)
+plt.subplot(211)
 plt.plot(tData, tDataY, 'or', markersize=3)
 plt.title('Random photon data from poisson distribution')
 plt.xlabel('time (sec)')
 plt.text(tData[0], 0.05, r'Gamma = ' + str(gamma) + r'(photons/sec)')
 plt.text(tData[0], 0.04, r'Delta time = ' + str(dt) + r'(sec)')
-plt.text(tData[0], 0.03, r'Chosen # of photons = ' + str(kTest))
+plt.text(tData[0], 0.03, r'Random generator seed = ' + str(args[3]))
+plt.text(tData[0], 0.02, r'Chosen # of photons = ' + str(kTest))
+
+plt.subplot(212)
+plt.hist(tData, bins = dt, facecolor='green')
+plt.xlabel('time(sec)')
+plt.ylabel('# of photons per sec')
 
 plt.savefig('rPoissonData15x5.png')
 plt.show()
